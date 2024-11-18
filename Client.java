6496c1a23f1fe8feaf9c1fe12757afac8b9b5b1d
seecharranjay <114945993+seecharranjay@users.noreply.github.com>
2024-11-17 21:10:50 -0500

    initializations based on test cases

--- Client.java
+++ Client.java
@@ -3,24 +3,6 @@ import java.net.*;
 
 public class Client {
     private Socket socket;
-    private PrintWriter out;
-
-    public Client(String host, int port) throws IOException {
-        socket = new Socket(host, port);
-        out = new PrintWriter(socket.getOutputStream(), true); 
-    }
-
-    public Socket getSocket() {
-        return socket;
-    }
-
-    public void handshake() {
-        out.println("12345");
-    }
-
-    public void disconnect() throws IOException {
-        out.close();
-        socket.close();
-    }
+    private PrintWrited out;
+    private BufferedReader in;
 }
-
