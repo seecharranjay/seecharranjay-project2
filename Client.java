a5450f12ed3b49ebf6dcbd032a3be499bc945cc6
seecharranjay <114945993+seecharranjay@users.noreply.github.com>
2024-11-17 21:40:55 -0500

    using lecture notes to try and help me

--- Client.java
+++ Client.java
@@ -2,14 +2,35 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+    /*
     private Socket socket;
     private PrintWrited out;
     private BufferedReader in;
 
-
+    
     public void disconnect() {
         out.close();
         in.close();
         socket.close();
     }
+    */
+   
+    Socket sock=null;        
+    try{
+        sock = new Socket("localhost",2021);
+    }catch(Exception e){
+        System.err.println("Cannot Connect");
+        System.exit(1);
+    }
+
+    try{
+        PrintWriter pw = new PrintWriter(sock.getOutputStream());
+        pw.println("HelloWorld");
+        pw.close(); //close the stream
+        sock.close();//close the socket
+    }catch(Exception e){
+        System.err.print("IOException");
+        System.exit(1);
+    }
 }
+
